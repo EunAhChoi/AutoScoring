63d02a8cf60ea46186a90c67bf3f15c851fa4a3a
ChanYi <chanyinara@naver.com>
2019-11-30 15:24:23 +0900

    Combine with flask done

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ answerList = []
 trueAnswer = []
 studentAnswer = []
 main_shape = ()
-state = None
+
 
 @app.route("/")
 def index():
@@ -139,9 +139,10 @@ def main_frame():
 # 라디오 버튼. 테스트 타입 정하기
 @app.route("/test_type", methods=['GET', 'POST'])
 def testType():
+    global state
     if request.method == "POST":
         state = request.form.get("myradio")
-        return render_template('mainframe.html')
+        return redirect('/mainframe')
 
 
 ## testSheet ##########################
@@ -168,7 +169,7 @@ def upload_EmptySheet():
 
         testSheet = cv2.cvtColor(testSheet, cv2.COLOR_RGB2GRAY)
 
-        return render_template('mainframe.html')
+        return redirect('/mainframe')
 
 
 ## AnswerSheet ####################################
@@ -273,9 +274,9 @@ def upload_AnswerSheet():
                 # +- 5 해서 좀 넓게 짤리게 했음. 인식 잘되게
                 img.append(answerSheet[minY-5:maxY+5, minX-5:maxX+5])    # img == 최종 답안 단어들의 이미지를 저장한 리스트
 
-
-        if state == 1:
-
+        print("state 는 :", state)
+        if state == "1":
+            print("state 1에 들어왔다!!")
             for i in range(0, len(img)):
                     cv2.imwrite("/Users/hcy/Desktop/GP/trueAnswer/"+""+str(i) + ".jpg", img[i])
 
@@ -299,7 +300,7 @@ def upload_AnswerSheet():
                 cv2.imwrite("/Users/hcy/Desktop/GP/trueAnswer/"+""+str(i) + ".jpg", img[i])
 
 
-        if state == 2:
+        if state == "2":
             print("#######################################")
             # 시험지와 답안지 비교해서 답을 짤라서 각각 저장
             for i in range(0, len(img)):
@@ -328,20 +329,166 @@ def upload_AnswerSheet():
             print("----------------------------")
             print("Extract Complete!!!") 
 
-        return render_template('mainframe.html')  
+        return redirect('/mainframe')
 
 
 ## StudentSheet ####################################
 # file이 submit되면 전달되는 페이지
 # mainframe.html에서 form이 제출되면 testSheet_uploaded로 옮겨지게 되어 있음
-@app.route('/AnswerSheet_uploaded', methods = ['GET','POST'])
-def upload_AnswerSheet():
-    global position, answerList, answerSheet
+@app.route('/StudentSheet_uploaded', methods = ['GET','POST'])
+def upload_StudentSheet():
+    global studentSheet
     if request.method == 'POST': # POST 방식으로 전달된 경우
-        f = request.files['AnswerSheet']
+        f = request.files['StudentSheet']
         # uploads 폴더에 저장
-        f.save("./uploads/AnswerSheet/" + secure_filename(f.filename))
+        f.save("./uploads/StudentSheet/" + secure_filename(f.filename))
 
+        color = cv2.imread("./uploads/StudentSheet/"+f.filename)
+        studentSheet = cv2.imread("./uploads/StudentSheet/"+f.filename)
+        
+        if not (os.path.isdir("/Users/hcy/Desktop/GP/answer")):
+            os.makedirs(os.path.join("/Users/hcy/Desktop/GP/answer"))
+
+        # 2차원 이미지 (흑백) -> 3차원 이미지 (컬러)
+        studentSheet = Perspective.point(studentSheet, main_shape)
+
+        studentSheet = cv2.cvtColor(studentSheet, cv2.COLOR_RGB2GRAY)
+
+        diff = cv2.absdiff(testSheet, studentSheet)
+        #mask = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+        mask = cv2.cvtColor(diff, cv2.COLOR_BAYER_BG2GRAY)
+
+        ret,img_binary=cv2.threshold(mask, 110,255,cv2.THRESH_BINARY)
+        cv2.imwrite("/Users/hcy/Desktop/result7.png", img_binary)
+
+        cnts= cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+        # 정답지 세로로 분리
+        x1, y1, h1 = 0, 0, 0
+        num = 0
+        count = 0
+        startX, startY = 0, 0
+        img2 = []
+        row2 = []
+        row2.append([])
+        box2 = []
+        for c in cnts:
+            (x, y, w, h) = cv2.boundingRect(c)
+            box2.append(c)
+            if count == 0:
+                row2[num].append(count)
+                pass
+            else:
+                if abs(y1 - y) < 20:
+                    row2[num].append(count)
+                else:
+                    num = num + 1
+                    row2.append([])
+                    row2[num].append(count)
+            y1 = y
+            count += 1
+
+        # 정답지 세로로 분리된 것 중에 가로로 분리할 수 있는지 확인
+        a, b, c, d = 0, 0, 0, 0
+        for i in range(0, len(row2)):
+            minX = 10000
+            minY = 10000
+            maxX = 0
+            maxY = 0
+
+            for j in range(0, len(row2[i])):
+                a, b, c, d = cv2.boundingRect(box2[row2[i][j]])
+                if minX > a:
+                    minX = a
+                if maxY < b + d:
+                    maxY = b + d
+                if maxX < a + c:
+                    maxX = a + c
+                if minY > b:
+                    minY = b
+            if abs(maxY - minY) < 5:
+                pass
+            else:
+                img2.append(studentSheet[minY-5:maxY+5, minX-5:maxX+5])  # img == 최종 답안 단어들의 이미지를 저장한 리스트
+                pos = []
+                pos.append(minY)
+                pos.append(maxY)
+                pos.append(minX)
+                pos.append(maxX)
+                position.append(pos)
+
+        if state == "1":
+                
+            for i in range(len(img2)):
+                result = pytesseract.image_to_string(img2[i],config='--psm 6')
+                studentAnswer.append(result)
+
+            for i in range(len(studentAnswer)):
+                print(studentAnswer[i] + "확인")
+
+            if not (os.path.isdir("/Users/hcy/Desktop/GP/answerSheet")):
+                os.makedirs(os.path.join("/Users/hcy/Desktop/GP/answerSheet"))
+            f = open("/Users/hcy/Desktop/GP/answerSheet/answerwordList.txt","w",-1,"utf-8")
+            for i in range(len(studentAnswer)):
+                f.write(studentAnswer[i]+"\n")
+            f.close()
+            
+            for i in range(0, len(img2)):
+                cv2.imwrite("/Users/hcy/Desktop/GP/answer/"+""+str(i) + ".jpg", img2[i])
+
+        if state == "2":
+                
+            for i in range(0, len(img2)):
+                cv2.imwrite("/Users/hcy/Desktop/GP/answer/"+""+str(i) + ".jpg", img2[i])
+            os.chdir("/Users/hcy/Desktop/GP/src/")
+            os.system("python3 main.py")           # main.py 실행하면 answerImage에 있는 폴더 모두 실행, txt파일에 정답 저장
+
+            # 학생 답지에서 추출한 답들. main.py에서 끌어다 왔음.
+            with codecs.open('/Users/hcy/Desktop/GP/answerSheet/answerwordLists.txt','r') as r:
+                while(1):
+                    line = r.readline()
+                    try:escape=line.index('\n')
+                    except:escape=len(line)
+
+                    if line:
+                        studentAnswer.append(line[0:escape].replace(" ",""))
+                    else:
+                        break
+            r.close()
+
+            print(studentAnswer)
+
+        print("###########################################")
+        score = len(studentAnswer)
+        print("score : ",score)
+        correctNum = np.zeros(len(trueAnswer))
+        for i in range(0,len(studentAnswer)):
+            print("true: %s, stud: %s" %(trueAnswer[i],studentAnswer[i]))
+            if studentAnswer[i] == trueAnswer[i] :
+                correctNum[i] = 1
+                
+            else :
+                score = score - 1   		
+            
+        print(score)
+        print(correctNum)
+        #color = cv2.imread("./uploads/StudentSheet/"+f.filename)
+        for i in range(0,len(correctNum)):
+            if(correctNum[i] == 1):
+                print("correct!")
+                #print(img2[i][0],img2[i][1])
+                cv2.circle(color,(int((position[i][3]+position[i][2])/2),int((position[i][0]+position[i][1])/2)),30,(0,0,255),5)
+                #cv2.circle(studentSheet,(int(x+w/2),int(y-h/2)),30,(0,0,255),-1)
+            else:
+                cv2.putText(color," / ", (int((position[i][3]+position[i][2])/2)-70,int((position[i][0]+position[i][1])/2)+35), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 255), 5, cv2.LINE_AA)
+
+        cv2.putText(color,str(score) + " / " + str(len(correctNum)), (1070,1950),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),5,cv2.LINE_AA)
+        color = cv2.resize(color,(850,850))
+        cv2.imwrite("/Users/hcy/Desktop/GP/AutoScoring/result/Result.jpg",color)
+        # cv2.circle(img,(447,63), 63, (0,0,255), -1)
+
+        return redirect('/mainframe')
 
 
 if __name__ == '__main__':
